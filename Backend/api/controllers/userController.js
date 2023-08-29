4b35e7f78d2bd8150dc3467b73976a9bb84e5e10
Monika <monirathore125@gmail.com>
2023-08-29 09:56:00 +0530

    register work

--- Backend/api/controllers/userController.js
+++ Backend/api/controllers/userController.js
@@ -1,14 +1,70 @@
 import userModel from '../models/userModel.js';
-import userRoute from '../routes/userRoute.js'
+import hashpassword from '../helpers/userHelper.js';
+
 
 export const userRegisterController = async (req, res, next) => {
-    const { firstname, lastname, email, phone, address, password, conformpassword } = req.body;
+    const { firstname, lastname, email, phone, address  ,password, conformpassword  } = req.body;
+  
     try {
         /** Validation All Entites */
-        if (!firstname && !lastname && !email && !phone && !address && !password && !conformpassword) {
-            return result.status(403).write({ success: false, message: "All Data are required" })
+        if (!firstname) {
+            return res.status(403).write({ success: false, message: "Firstname is required" })
         }
-        console.log(req.body);
+        if (!lastname) {
+            return res.status(403).write({ success: false, message: "Lastname is required" })
+        }
+        if (!email) {
+            return res.status(403).write({ success: false, message: "Email is required" })
+        }
+        if (!phone) {
+            return res.status(403).write({ success: false, message: "Phone Number  is required" })
+        }
+
+        if (!address) {
+            return res.status(403).write({ success: false, message: "Address is required" })
+        }
+        if (!password) {
+            return res.status(403).write({ success: false, message: "Password is required" })
+        }
+        if (!conformpassword) {
+            return res.status(403).write({ success: false, message: "ComformPassword is required" })
+        }
+    
+
+        // test purpose 
+
+        console.log(firstname," ",lastname," ",email," ",address," ",phone," ",password," ",conformpassword);
+        /**User Exist  */
+
+        const userExist = await userModel.findOne({
+             email
+        });
+
+        if(userExist){
+            return res.status(409).json({
+                success:true,
+                msg : "User all ready Exist with this email"
+            });
+        }
+        
+
+        /**New User Register */
+        
+        /**Matching Password */
+
+        var isMatchPassword = (password === conformpassword);
+        if (!isMatchPassword) {
+            return res.status(402).json({
+                success: true,
+                error: 'password does not match'
+            });
+        }
+
+        /** Encrpting password */
+        // const encrptPass = await hashpassword(password);
+
+
+
     } catch (error) {
         console.log("Error in User Register Controller", error);
         return res.status(500).json({
